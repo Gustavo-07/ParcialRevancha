2a68d0d29adfe2ad7bf2a141638da485d60e4ff6
Royber <roybermanjarrez@gmail.com>
2020-11-17 19:59:59 -0500

    Primer Commit

---
+++ TestDominio/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestDominio
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
